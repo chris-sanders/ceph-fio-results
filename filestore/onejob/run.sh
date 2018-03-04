437004a1ed81c0c74b8f18cdfc8d9294e0863e3e
Chris Sanders <sanders.chris@gmail.com>
2018-03-04 10:10:27 -0600

    Initial commit

---
+++ filestore/onejob/run.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+CFG=$1
+
+date
+export RW=read
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=read
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randread
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randread
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=write
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=write
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randwrite
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randwrite
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=rw
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=rw
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio $CFG --output=$RW-$BLOCK_SIZE.out
+date
+
---
+++ hdd/run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+date
+export RW=read
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=read
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randread
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randread
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=write
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=write
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randwrite
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=randwrite
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=rw
+export BLOCK_SIZE=4k
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
+export RW=rw
+export BLOCK_SIZE=1M
+rm -fr /dev/shm/fio/$RW-$BLOCK_SIZE
+mkdir -p /dev/shm/fio/$RW-$BLOCK_SIZE
+fio hdd.fio --output=$RW-$BLOCK_SIZE.out
+date
+
